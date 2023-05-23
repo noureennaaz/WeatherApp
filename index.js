727642feb1c5531893f4c0240777975cf6bc867e
noureennaaz <noureennaaz09@gmail.com>
2023-05-24 00:24:08 +0530

    Added tailwind

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-int 
+const API_KEY="473f54186294f1278a516ff968aaa1f3";
+async function fetchWeatherData()
+{
+    let city='jabalpur';
+
+    try{
+        const respose=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+
+        const data= await respose.json();
+        console.log("Weather data Is ->"+ data);
+        renderInfo(data);
+
+    }
+    catch(err)
+    {
+        console.log("Error found ", err);
+
+    }
+    
+
+}
+let info=document.createElement('p');
+function renderInfo(data)
+{
+    info.textContent=`${data?.main?.temp.toFixed(2)}  °C`;
+    document.body.appendChild(info);
+
+}
